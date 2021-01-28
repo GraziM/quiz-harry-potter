ddffe51386e1eb478825d6d2475273c0385a8d88
Grazielle Alves <graziellemontenegro@hotmail.com>
2021-01-28 20:44:30 -0300

    Adicionando default

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-export function QuizDaGaleraPage(){
-    return(
-        <div>
-            Próxima aula
-        </div>
-    )
+export default function QuizDaGaleraPage() {
+  return (
+    <div>
+      Próxima aula
+    </div>
+  );
 }
